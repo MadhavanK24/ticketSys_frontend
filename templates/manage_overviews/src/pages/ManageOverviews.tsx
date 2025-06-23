7856ba077a40d597d4f122954773a7cc43004009
rithekasunderraman <rithekasunderraman@gmail.com>
2025-06-23 22:21:10 +0530

    feat: management

---
+++ templates/manage_overviews/src/pages/ManageOverviews.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import OverviewTable from '../components/OverviewTable';
+import '../styles/manage.css';
+
+const ManageOverviews = () => {
+  return (
+    <div className="container">
+      <aside className="sidebar">
+        <h2>Manage</h2>
+        <ul>
+          <li>Users</li>
+          <li className="active">Overviews</li>
+          <li>Roles</li>
+          <li>Templates</li>
+        </ul>
+      </aside>
+      <main className="main-content">
+        <OverviewTable />
+      </main>
+    </div>
+  );
+};
+
+export default ManageOverviews;
