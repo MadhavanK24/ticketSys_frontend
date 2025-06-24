5ae26fddb6eee2cf0cf10c418103dbcb7faf3a83
samyukthaa-aravinth <samyukthaa.aravinth@gmail.com>
2025-06-24 11:06:09 +0530

    minor changes in import warning

--- templates/manage_overviews/src/components/OverviewTable.tsx
+++ templates/manage_overviews/src/components/OverviewTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import 'react';
 import '../styles/manage.css';
 
 const data = [
--- templates/manage_overviews/src/pages/ManageOverviews.tsx
+++ templates/manage_overviews/src/pages/ManageOverviews.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import 'react';
 import OverviewTable from '../components/OverviewTable';
 import '../styles/manage.css';
 
